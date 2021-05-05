2ddeedc3bcc151a2bf26f43fdc576ea97d18b7c5
dddd <dddd>
2021-05-05 22:17:52 +0300

    Настройки для heroku

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -146,5 +146,6 @@ EMAIL_PORT = EMAIL_PORT
 EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Место, где лежит статика
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
