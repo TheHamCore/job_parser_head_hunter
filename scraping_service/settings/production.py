02bbcbfb08656cc3198b48145211e0c2fba1d41c
dddd <dddd>
2021-05-06 20:19:23 +0300

    Настройки для heroku

--- scraping_service/settings/production.py
+++ scraping_service/settings/production.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'scraping_service.urls'
 
 TEMPLATES = [
-    {#
+    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
